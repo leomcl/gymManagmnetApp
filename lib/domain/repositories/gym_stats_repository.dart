fa20ff711121e804d13fab5b63a3e7b5baf6663a
Leo Mclaughlin <leo.mcl616@gmail.com>
2025-03-03 21:48:19 +0000

    Added get hourly attendace function.

--- lib/domain/repositories/gym_stats_repository.dart
+++ lib/domain/repositories/gym_stats_repository.dart
@@ -2,6 +2,9 @@ abstract class GymStatsRepository {
   /// Returns a stream of the current number of people in the gym
   Stream<int> getCurrentGymOccupancy();
 
+  /// Returns hourly attendance data for the current day
+  Future<Map<int, int>> getHourlyAttendanceForToday();
+
   /// Returns hourly attendance data for the chart
   /// TODO: Implement this
   // Future<List<Map<String, dynamic>>> getHourlyAttendance([int? daysBack]);
